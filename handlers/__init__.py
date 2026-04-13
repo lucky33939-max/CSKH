4bb93dc12baac68f063811baeb7d03f41e7b528b
lucky33939-max <lucky33939@gmail.com>
2026-04-13 09:43:19 +0700

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,15 +1,8 @@
-from aiogram import Dispatcher
-
 from .start import router as start_router
-from .profile import router as profile_router
 from .shop import router as shop_router
-from .wallet import router as wallet_router
-from .orders import router as orders_router
+from .admin import router as admin_router
 
-
-def register_all_routers(dp: Dispatcher):
+def register_all(dp):
     dp.include_router(start_router)
-    dp.include_router(profile_router)
     dp.include_router(shop_router)
-    dp.include_router(wallet_router)
-    dp.include_router(orders_router)
+    dp.include_router(admin_router)
