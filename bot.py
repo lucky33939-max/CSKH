a3e81201b90e24881be87b9a14d2acda947b3050
lucky33939-max <lucky33939@gmail.com>
2026-04-13 09:50:15 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,32 @@
 import asyncio
 from aiogram import Bot, Dispatcher
+from aiohttp import web
 from config import TOKEN
 from handlers import register_all
+from webhook import webhook
 
-async def main():
+async def start_bot():
     bot = Bot(token=TOKEN)
     dp = Dispatcher()
 
     register_all(dp)
 
     await dp.start_polling(bot)
+
+async def start_web():
+    app = web.Application()
+    app.router.add_post("/webhook", webhook)
+
+    runner = web.AppRunner(app)
+    await runner.setup()
+    site = web.TCPSite(runner, "0.0.0.0", 8080)
+    await site.start()
+
+async def main():
+    await asyncio.gather(
+        start_bot(),
+        start_web()
+    )
 
 if __name__ == "__main__":
     asyncio.run(main())
