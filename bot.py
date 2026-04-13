cc7408a1e72979418c25fe92265eb987333d514e
lucky33939-max <lucky33939@gmail.com>
2026-04-13 12:46:07 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,6 +14,7 @@ from aiogram.types import (
     InlineKeyboardButton,
 )
 from aiogram.utils.keyboard import InlineKeyboardBuilder
+from aiogram.exceptions import TelegramBadRequest
 
 from db import (
     init_db,
